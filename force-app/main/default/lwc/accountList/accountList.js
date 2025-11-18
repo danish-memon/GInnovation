5d104049b0142dfd664ebb1f42a1caf33c3ccea2
Danish <danish.memon@globant.com>
2025-11-18 16:54:37 +0530

    GInnovation Release V1

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,8 +1,27 @@
 import { LightningElement, wire, track } from 'lwc';
-import getTopAccounts from '@salesforce/apex/AccountController.getTopAccounts';
+import fetchTopAccounts from '@Salesforce/apex/AccountController.fetchTopAccounts';
 export default class AccountList extends LightningElement {
     @track accounts;
-    @wire(getTopAccounts) wiredAccounts({error, data}){
-        if(data) this.accounts = data;
+    @track error;
+    // Reactive parameters to pass into Apex
+    minRevenue = 0;       // default: no revenue filter
+    limitSize = 20;       // default now matches new Apex default
+    industry = '';        // empty = no industry filter
+    @wire(fetchTopAccounts, {
+        minRevenue: '$minRevenue',
+        limitSize: '$limitSize',
+        industry: '$industry'
+    })
+    wiredAccounts({ error, data }) {
+        if (data) {
+            this.accounts = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.accounts = undefined;
+        }
+    }
+    get hasAccounts() {
+        return this.accounts && this.accounts.length > 0;
     }
 }
